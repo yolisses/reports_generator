fd3040a7927ce6d817a35a63516991dfcf2ee0b6
Ulisses <m.stunik@gmail.com>
2021-10-19 07:38:40 -0300

    sum users buys and count foods

--- lib/parser.ex
+++ lib/parser.ex
@@ -2,7 +2,7 @@ defmodule ReportsGenerator.Parser do
   def parse_file(filename) do
     "reports/#{filename}"
     |> File.stream!
-    |> Stream.map &parse_line/1
+    |> Stream.map(&parse_line/1)
   end
 
   defp parse_line(line) do
--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -3,14 +3,27 @@ defmodule ReportsGenerator do
 
   def build(filename) do
     filename
-    |> Parser.parse_file
-    |> Enum.reduce(%{}, fn line, report ->
-      sum_values(line, report)
+    |> Parser.parse_file()
+    |> Enum.reduce(get_accumulator(), fn line, acc ->
+      sum_values(line, acc)
     end)
   end
 
-  defp sum_values([id, _food_name, price], report) do
-    Map.put(report, id, get_sum({report[id], price}))
+  def get_accumulator do
+    %{users: %{}, foods: %{}}
+  end
+
+  def fetch_higher_cost(report) do
+    Enum.max_by(report, fn {_key, value} -> value end)
+  end
+
+  defp sum_values(
+         [id, food_name, price],
+         %{foods: foods, users: users} = report
+       ) do
+    users = Map.put(users, id, get_sum({users[id], price}))
+    foods = Map.put(foods, food_name, get_sum({foods[food_name], 1}))
+    %{report | users: users, foods: foods}
   end
 
   defp get_sum({nil, new}), do: new
